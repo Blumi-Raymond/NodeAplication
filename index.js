d4b655cd7cfe68a686c3de452e209d8931995475
Blumi Raymond <Blumi5807@gmail.com>
2025-03-09 16:38:43 +0200

    get function

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from 'dotenv'
+import cors from 'cors'
 import renderApi from '@api/render-api';
 
-renderApi.auth('rnd_wra223DHBda9dyyheGWZCJV28Tb3');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+
+const app = express();
+const port = 3001
+dotenv.config()
+app.use(bodyParser.json())
+app.use(cors())
+app.get('', (req, res) => {
+  renderApi.auth(process.env.RENDER_API_KEY);
+  renderApi.listServices({ includePreviews: 'true', limit: '20' })
+    .then(({ data }) => {
+      console.log(data)
+      return res.status(200).send({ data: data })
+    }
+    )
+    .catch(err => console.error(err));
+
+
+})
+app.listen(port, () => {
+  console.log(`my application is listening on http://localhost:${port}`);
+})
